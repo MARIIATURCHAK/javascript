6a7b2b9e6c05c802122bc0903aef2ba344797f6f
MariiaTurchak <mariia8v@gmail.com>
2021-12-06 00:30:44 +0200

    hw10

---
+++ Lesson10/hw10/hw10.js
@@ -0,0 +1,53 @@
+// - Создать произвольный елемент с id = text.  Используя JavaScript, сделайте так,
+// чтобы при клике на кнопку исчезал элемент с id="text".
+
+// let textDisappears = document.getElementById('text');
+// let button = document.getElementById('button');
+// button.onclick = function () {
+//     textDisappears.style.display = 'none';
+// }
+
+//     - Создайте кнопку, при клике на которую, она будет скрывать сама себя.
+// let button = document.getElementById('button');
+// button.onclick = function () {
+//     button.style.display = 'none';
+// }
+// // or
+// document.getElementById('button').onclick = function () {
+//     document.getElementById('button').hidden = true;
+// }
+// - створити інпут який приймає вік людини та кнопку яка підтверджує дію.При натисканні на кнопку
+// зчитати інформацію з інпуту та перевірити вік чи меньше він ніж 18, та повідомити про це користувача
+// document.getElementById('button').onclick = function () {
+//     let age = document.getElementById('age').value;
+//     {
+//         if (age >= 18) {
+//             alert('Welcome');
+//         } else {
+//             alert('Sorry, but you cannot enter')
+//         }
+//     }
+// }
+
+// - Создайте меню, которое раскрывается/сворачивается при клике
+//
+// let menuElement = document.getElementById('menu')
+// let listElement = document.getElementById('list');
+//
+// menuElement.onmouseover = function () {
+//     listElement.style.display = 'none'
+// };
+// menuElement.onmouseleave = function () {
+//     listElement.style.display = 'block';
+// }
+
+let menuElement = document.getElementById('menu');
+let listElement = document.getElementById('list');
+
+listElement.onclick = function () {
+    menuElement.classList.toggle('hidden');
+};
+
+// - Создать список комментариев , пример объекта коментария - {title : 'lorem', body:'lorem ipsum dolo sit ameti'}.
+//     Вывести список комментариев в документ, каждый в своем блоке.
+//     Добавьте каждому комментарию по кнопке для сворачивания его body.
