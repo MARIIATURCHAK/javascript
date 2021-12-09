8e03fe91d80a3cd7034389f8d34d76720ef6222b
MariiaTurchak <mariia8v@gmail.com>
2021-12-09 22:20:28 +0200

    hw11

--- Lesson11/hw11/hw11.js
+++ Lesson11/hw11/hw11.js
@@ -23,22 +23,22 @@
 // -створити форму з інпутами для model,type та volume автівки.
 //     при відпарвці форми об'єкти зберігаються в масиві в локальному сховищі.
 
-// let form =  document.forms;
-// let model =  form.carForm.model;
-// console.log(model.value)
-// let type =  form.carForm.type;
-// let volume =  form.carForm.volume;
-// let button = document.getElementById('button');
-// let key = 'key';
-//
-// let click = (modelCar, typeCar, volumeCar) => {
-//     let arrayCar = JSON.parse(localStorage.getItem(key)) || [];
-//
-//     arrayCar.push({modelCar, typeCar, volumeCar})
-//     localStorage.setItem(key, JSON.stringify(arrayCar));
-// }
-// button.addEventListener('click', function (){
-//     click(model.value, type.value, volume.value);
-// })
+let form =  document.forms;
+let model =  form.carForm.model;
+console.log(model.value)
+let type =  form.carForm.type;
+let volume =  form.carForm.volume;
+let button = document.getElementById('button');
+let key = 'key';
+
+let click = (modelCar, typeCar, volumeCar) => {
+    let arrayCar = JSON.parse(localStorage.getItem(key)) || [];
+
+    arrayCar.push({modelCar, typeCar, volumeCar})
+    localStorage.setItem(key, JSON.stringify(arrayCar));
+}
+button.addEventListener('click', function (){
+    click(model.value, type.value, volume.value);
+})
 
 
