1dea78f109734e7cb0f3a26a6721e941b1990599
MariiaTurchak <mariia8v@gmail.com>
2021-12-13 16:59:24 +0200

    cw12

--- Lesson12/cw12/cw12.js
+++ Lesson12/cw12/cw12.js
@@ -1,4 +1,3 @@
-
 // Отримати відповідь з цього ресурсу відповідь, та вивести в документ як в прикладі на занятті
 // https://jsonplaceholder.typicode.com/posts
 //     зробити кнопку до кожного поста. при кліку на яку виводяться в окремий блок всі коментарі поточного поста
@@ -15,7 +14,36 @@ fetch('https://jsonplaceholder.typicode.com/posts')
                       <h3> ${post.title}</h3>
                       <h4> ${post.body}</h4>
                     `;
-            wrap.appendChild(divPost)
-            document.body.appendChild(wrap)
-        }
+            let button = document.createElement('button');
+            button.classList.add('btn');
+            button.innerText = 'click';
+            button.onclick = (id) => {
+                fetch('https://jsonplaceholder.typicode.com/posts/' + post.id + '/comments')
+                    .then((response) => response.json())
+                    .then(comments => {
+                        for (const comment of comments) {
+                            if (post.id === comment.postId) {
+                                let divComment = document.createElement('div');
+                                divComment.classList.add('divComment');
+                                divComment.innerHTML = `
+                      <h2> ${comment.id}</h2>
+                      <h3> ${comment.name}</h3>
+                      <h3> ${comment.email}</h3>
+                      <h4> ${comment.body}</h4>
+                    `;
+
+                                wrap.appendChild(divComment)
+                            }
+                            button.disabled = true;
+                        }
+                    })
+            }
+                divPost.appendChild(button);
+                wrap.appendChild(divPost);
+                document.body.appendChild(wrap);
+            }
     })
+
+
+
+
